f62811eb51f2406a31ebd4c826172187e5194e8b
DimitriVld <Dimitri.vildina@gmail.com>
2020-05-13 14:27:19 +0200

    Style and responsive home page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,3 +2,22 @@ import $ from 'jquery';
 
 $(".nav_container").load("./components/nav-component.html");
 $(".footer_container").load("./components/footer-component.html");
+
+$('.slick').slick({
+    infinite: false,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: .5,
+            centerMode: false,
+          }
+        }
+      ]
+});
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@
 require("./assets/styles/app.scss");
 
 // SCRIPTS
+require("./assets/scripts/slick.min.js");
 require("./assets/scripts/main.js");
